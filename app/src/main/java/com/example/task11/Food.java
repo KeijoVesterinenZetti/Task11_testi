7e9ed708157e31cad3f109a3c716ecb99dfd8039
KeijoVesterinenZetti <129832006+KeijoVesterinenZetti@users.noreply.github.com>
2023-04-12 23:41:08 +0300

    Task11

---
+++ app/src/main/java/com/example/task11/Food.java
@@ -0,0 +1,44 @@
+package com.example.task11;
+
+import java.util.Comparator;
+import java.io.Serializable;
+public class Food implements Serializable {
+
+    private String name;
+    private String memo;
+
+    private int timeCounter;
+    private static final long serialVersionUID = 2432343461L;
+
+    public Food(String name, String memo, int timeCounter){
+        this.name=name;
+        this.memo=memo;
+        this.timeCounter=timeCounter;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getMemo(){
+        return  memo;
+    }
+    public int getTimeCounter(){ return timeCounter;}
+
+    //comparator
+    public static Comparator<Food> nameComparator = new Comparator<Food>() {
+        @Override
+        public int compare(Food us1, Food us2) {
+            return us1.getName().compareTo(us2.getName());
+        }
+    };
+    public static Comparator<Food> timeComparator = new Comparator<Food>() {
+        @Override
+        public int compare(Food us1, Food us2) {
+            return us1.getTimeCounter() - (us2.getTimeCounter());
+        }
+    };
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+}
